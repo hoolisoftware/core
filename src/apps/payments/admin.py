72e2a21c39291e84f74a3e88887e345fd3aae942
Ilkhan <ilhanchanel20002@gmail.com>
2023-10-24 12:55:12 +0300

    Added payments application

---
+++ src/apps/payments/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.PaymentCurrecy)
+class PaymentCurrencyAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'code'
+    )
+
+
+@admin.register(models.PaymentMethod)
+class PaymentMethodAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'currency'
+    )
+
+
+@admin.register(models.Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'method',
+        'amount',
+        # TODO Order
+    )
---
+++ src/apps/payments/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class PaymentCurrecy(models.Model):
+    class Meta:
+        verbose_name = 'Валюта оплаты'
+        verbose_name_plural = 'Валюты оплаты'
+
+    name = models.CharField('Название', max_length=64)
+    code = models.CharField('Код', max_length=8)
+
+    def __str__(self):
+        return f'{self.name} ({self.code})'
+
+
+class PaymentMethod(models.Model):
+    class Meta:
+        verbose_name = 'Метод оплаты'
+        verbose_name_plural = 'Методы оплаты'
+
+    currency = models.ForeignKey(PaymentCurrecy, verbose_name='Валюта оплаты', on_delete=models.CASCADE) # noqa
+    name = models.CharField('Название', max_length=64)
+    requisites = models.TextField('Реквизиты', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} {self.currency}'
+
+
+class Payment(models.Model):
+    class Meta:
+        verbose_name = 'Транзакция'
+        verbose_name_plural = 'Транзакции'
+
+    method = models.ForeignKey(PaymentMethod, verbose_name='Метод оплаты', on_delete=models.CASCADE) # noqa ignore
+    amount = models.PositiveIntegerField('Сумма')
+    order = None  # TODO Order
+
+    def __str__(self):
+        return f'{self.amount} {self.method.currency}'
